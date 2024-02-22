c6c672bfb4b2562a953f97d410396ae00b11cc86
Beosti <ziroxismail@gmail.com>
2024-02-22 23:41:13 +0100

    changed kido overlay

--- src/main/java/com/yuanno/soulsawakening/client/overlay/KidoOverlay.java
+++ src/main/java/com/yuanno/soulsawakening/client/overlay/KidoOverlay.java
@@ -1,5 +1,8 @@
 package com.yuanno.soulsawakening.client.overlay;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.yuanno.soulsawakening.ability.api.Ability;
+import com.yuanno.soulsawakening.api.Beapi;
 import com.yuanno.soulsawakening.data.ability.AbilityDataCapability;
 import com.yuanno.soulsawakening.data.ability.IAbilityData;
 import net.minecraft.client.Minecraft;
@@ -9,6 +12,8 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.ArrayList;
 
 @OnlyIn(Dist.CLIENT)
 public class KidoOverlay extends AbstractGui {
@@ -20,9 +25,23 @@ public class KidoOverlay extends AbstractGui {
         {
             IAbilityData abilityData = AbilityDataCapability.get(Minecraft.getInstance().player);
             //System.out.println(abilityData.getSelectedAbility());
-            if (!abilityData.getAbilitiesInBar().isEmpty() && abilityData.getAbilitiesInBar().get(abilityData.getSelectionAbility()) != null) {
-                drawString(event.getMatrixStack(), Minecraft.getInstance().font, TextFormatting.BOLD + abilityData.getAbilitiesInBar().get(abilityData.getSelectionAbility()).getName(), 363, 195, -1);
-            }
+            MatrixStack matrixStack = event.getMatrixStack();
+            ArrayList<Ability> abilities = abilityData.getAbilitiesInBar();
+            int selectedAbilityInteger = abilityData.getSelectionAbility();
+            Ability selectedAbility;
+            Ability previousAbility;
+            Ability upComingAbility;
+            if (!(abilities.isEmpty() && abilities.get(selectedAbilityInteger) != null))
+                return;
+            selectedAbility = abilities.get(selectedAbilityInteger);
+            if (selectedAbilityInteger == 0)
+                previousAbility = abilities.get(abilities.size());
+            else
+                previousAbility = abilities.get(selectedAbilityInteger - 1);
+            if (selectedAbilityInteger >= abilities.size())
+                upComingAbility = abilities.get(0);
+            else
+                upComingAbility = abilities.get(selectedAbilityInteger + 1);
         }
     }
 }
