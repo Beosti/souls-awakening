12470b8253ec34f9db9253a7c2d7006877046129
Beosti <ziroxismail@gmail.com>
2024-03-31 00:38:17 +0100

    deleted the asauchi item

--- src/main/java/com/yuanno/soulsawakening/abilities/elements/fire/FireAttackAbility.java
+++ src/main/java/com/yuanno/soulsawakening/abilities/elements/fire/FireAttackAbility.java
@@ -8,7 +8,6 @@ import net.minecraft.entity.player.PlayerEntity;
 public class FireAttackAbility extends Ability implements IAttackAbility {
     public static final FireAttackAbility INSTANCE = new FireAttackAbility();
 
-
     public FireAttackAbility() {
         this.setName("Fire Attack");
         this.setDescription("Hitting an enemy puts them on fire");
--- src/main/java/com/yuanno/soulsawakening/init/ModItems.java
+++ src/main/java/com/yuanno/soulsawakening/init/ModItems.java
@@ -1,7 +1,6 @@
 package com.yuanno.soulsawakening.init;
 
 import com.yuanno.soulsawakening.Main;
-import com.yuanno.soulsawakening.items.AsauchiItem;
 import com.yuanno.soulsawakening.items.ShinaiItem;
 import com.yuanno.soulsawakening.items.ZanpakutoWakizashiItem;
 import com.yuanno.soulsawakening.items.blueprints.ZanpakutoItem;
@@ -29,6 +28,5 @@ public class ModItems {
     public static final RegistryObject<Item> ZANPAKUTO = ITEMS.register("zanpakuto", ZanpakutoItem::new);
     public static final RegistryObject<Item> ZANPAKUTO_WAKIZASHI = ITEMS.register("zanpakuto_wakizashi", ZanpakutoWakizashiItem::new);
     public static final RegistryObject<Item> SHINAI = ITEMS.register("shinai", ShinaiItem::new);
-    public static final RegistryObject<Item> ASAUCHI = ITEMS.register("asauchi", AsauchiItem::new);
 
 }
