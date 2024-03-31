226521c7152dee026d77909e9288b233073035dd
Beosti <ziroxismail@gmail.com>
2024-04-01 01:13:35 +0200

    adding all the spells for bakudo, realized I should also give normal entity reiatsu points...

--- src/main/java/com/yuanno/soulsawakening/abilities/kido/bakudo/SaiAbility.java
+++ src/main/java/com/yuanno/soulsawakening/abilities/kido/bakudo/SaiAbility.java
@@ -1,10 +1,13 @@
 package com.yuanno.soulsawakening.abilities.kido.bakudo;
 
 import com.yuanno.soulsawakening.ability.api.Ability;
+import com.yuanno.soulsawakening.ability.api.KidoAbility;
+import com.yuanno.soulsawakening.ability.api.interfaces.IEntityRayTrace;
 import com.yuanno.soulsawakening.ability.api.interfaces.IReiatsuAbility;
 import com.yuanno.soulsawakening.ability.api.interfaces.IShootAbility;
 import com.yuanno.soulsawakening.api.Beapi;
 import com.yuanno.soulsawakening.data.entity.EntityStatsCapability;
+import com.yuanno.soulsawakening.data.entity.IEntityStats;
 import com.yuanno.soulsawakening.init.ModEffects;
 import com.yuanno.soulsawakening.init.ModI18n;
 import net.minecraft.entity.LivingEntity;
@@ -15,7 +18,7 @@ import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
 
 // Seals an entity, the entity can break through easily
-public class SaiAbility extends Ability implements IShootAbility, IReiatsuAbility {
+public class SaiAbility extends KidoAbility implements IEntityRayTrace, IReiatsuAbility {
     public static final SaiAbility INSTANCE = new SaiAbility();
 
     public SaiAbility()
@@ -23,29 +26,19 @@ public class SaiAbility extends Ability implements IShootAbility, IReiatsuAbilit
         this.setName("Sai");
         this.setDescription("Restraints the entities arms");
         this.setMaxCooldown(10);
+        this.setIncantation("Ye thy stop moving Bakudo number 1 Sai");
         this.setSubCategory(SubCategory.BAKUDO);
     }
 
     @Override
-    public void onUse(PlayerEntity player)
-    {
-        RayTraceResult result = Beapi.rayTraceBlocksAndEntities(player, 4 + EntityStatsCapability.get(player).getReiatsuPoints());
-        if (result instanceof EntityRayTraceResult)
-        {
-            EntityRayTraceResult entityRayTraceResult = (EntityRayTraceResult) result;
-            if (entityRayTraceResult.getEntity() instanceof LivingEntity)
-            {
-                LivingEntity entity = (LivingEntity)entityRayTraceResult.getEntity();
-                if (EntityStatsCapability.get(player).getReiatsuPoints() > EntityStatsCapability.get(entity).getReiatsuPoints() * 2)
-                    entity.addEffect(new EffectInstance(ModEffects.SAI.get(), 200, 0, false, false));
-                else
-                    player.sendMessage(ModI18n.REIATSU_TARGET, Util.NIL_UUID);
-            }
-        }
-        else
-        {
-            player.sendMessage(ModI18n.NO_TARGET, Util.NIL_UUID);
+    public void somethingAtEntity(PlayerEntity player, LivingEntity entity) {
+        IEntityStats entityStatsPlayer = EntityStatsCapability.get(player);
+        if (!(entity instanceof PlayerEntity))
             return;
-        }
+        PlayerEntity target = (PlayerEntity) entity;
+        IEntityStats entityStatsTarget = EntityStatsCapability.get(target);
+        if (!(entityStatsPlayer.getReiatsuPoints() > entityStatsTarget.getReiatsuPoints() * 2))
+            return;
+
     }
 }
