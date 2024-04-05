da74c4ba9f21412694a9eef57fc2d4adb201ed0f
Beosti <ziroxismail@gmail.com>
2024-04-06 00:10:08 +0200

    working on the bakudo teacher

--- src/main/java/com/yuanno/soulsawakening/quests/Quest.java
+++ src/main/java/com/yuanno/soulsawakening/quests/Quest.java
@@ -20,6 +20,10 @@ public abstract class Quest extends ForgeRegistryEntry<Quest> {
     private QuestReward questReward;
     private QuestStart questStart;
 
+    public Quest()
+    {
+
+    }
 
     public void addObjectives(Objective... objectives)
     {
@@ -93,7 +97,6 @@ public abstract class Quest extends ForgeRegistryEntry<Quest> {
     public CompoundNBT save()
     {
         CompoundNBT nbt = new CompoundNBT();
-        nbt.putString("id", this.getRegistryName().toString());
         nbt.putString("title", this.title);
         nbt.putString("description", this.description);
         nbt.putString("rank", this.rank);
@@ -110,6 +113,13 @@ public abstract class Quest extends ForgeRegistryEntry<Quest> {
 
     public void load(CompoundNBT nbt)
     {
+        /*
+        this.title = nbt.getString("title");
+        this.description = nbt.getString("description");
+        this.rank = nbt.getString("rank");
+        this.inProgress = nbt.getBoolean("inProgress");
+
+         */
         ListNBT objectivesData = nbt.getList("objectives", Constants.NBT.TAG_COMPOUND);
         for (int i = 0; i < objectivesData.size(); i++)
         {
