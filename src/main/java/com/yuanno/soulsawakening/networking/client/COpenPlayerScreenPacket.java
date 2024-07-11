efc314d5143ab19f851ddef6421fb40cddb2d1a6
Beosti <ziroxismail@gmail.com>
2024-07-11 19:48:27 +0200

    changed: you can't open your player overview screen once a challenge started now;

--- src/main/java/com/yuanno/soulsawakening/networking/client/COpenPlayerScreenPacket.java
+++ src/main/java/com/yuanno/soulsawakening/networking/client/COpenPlayerScreenPacket.java
@@ -2,11 +2,14 @@ package com.yuanno.soulsawakening.networking.client;
 
 import com.yuanno.soulsawakening.data.entity.EntityStatsCapability;
 import com.yuanno.soulsawakening.data.entity.IEntityStats;
+import com.yuanno.soulsawakening.init.world.ModDimensions;
 import com.yuanno.soulsawakening.networking.PacketHandler;
 import com.yuanno.soulsawakening.networking.server.SOpenChallengeScreenPacket;
 import com.yuanno.soulsawakening.networking.server.SOpenPlayerScreenPacket;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.PacketBuffer;
+import net.minecraft.world.Dimension;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.fml.network.NetworkDirection;
 import net.minecraftforge.fml.network.NetworkEvent;
 
@@ -34,7 +37,13 @@ public class COpenPlayerScreenPacket
 			ctx.get().enqueueWork(() ->
 			{
 				PlayerEntity player = ctx.get().getSender();
-
+				if (!((ServerWorld) player.level).dimension().equals(ModDimensions.SOUL_SOCIETY)
+						&& !((ServerWorld) player.level).dimension().equals(ModDimensions.SOUL_SOCIETY_SHADOW)
+						&& !((ServerWorld) player.level).dimension().equals(ModDimensions.HUECO_MUNDO)
+						&& !((ServerWorld) player.level).dimension().equals(Dimension.OVERWORLD)
+						&& !((ServerWorld) player.level).dimension().equals(Dimension.END)
+						&& !((ServerWorld) player.level).dimension().equals(Dimension.NETHER))
+					return;
 				PacketHandler.sendTo(new SOpenPlayerScreenPacket(), player);
 			});
 		}
